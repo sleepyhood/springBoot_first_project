c439f91836a93e2f3fc32112e2540c20b0e67151
osw <smartosw@korea.ac.kr>
2024-07-24 01:28:21 +0900

    first commit 07.24

---
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.controller;
+
+// 컨트롤러 선언과 동시에 자동으로 임포트
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //--------------------------- ➊ 컨트롤러 선언
+
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) { //메서드 작성
+        // model 객체가 "홍팍" 값을 "username"에 연결해 웹 브라우저로 보냄
+        model.addAttribute("username", "홍팍");
+
+        return "greetings"; // greetings.mustache 파일 반환
+    }
+}
